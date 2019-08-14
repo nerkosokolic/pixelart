9df311b935b4cf1edf4ec29eb30c71c50842c304
nerkosokolic <nerkosokolic@gmail.com>
2019-08-14 14:23:03 +1000

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+colorInput = document.querySelector("#color-selection")
+colorButton = document.querySelector("#color-btn")
+
+var changeColor = function() {
+    alert("works")
+}
+
+colorButton.addEventListener('click', changeColor)
