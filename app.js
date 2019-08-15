da248834f4fd6e4e718007aeef9d70f48ce4be64
nerkosokolic <nerkosokolic@gmail.com>
2019-08-15 11:51:58 +1000

    refactoring pixel ID logic

--- app.js
+++ app.js
@@ -1,34 +1,27 @@
 
-    // window.onload = function() {
-    //     displayWindowSize()
-    // }
-
-    // when resizing the view the reallocatePixels no longer works!
-
-var displayWindowSize = function() {
+var displayWindowSize = () => {
     var w = document.documentElement.clientWidth;
     var h = document.documentElement.clientHeight;
-    var pixelSize = 20*20
-    var spacing = 10
-    var windowArea = (w-spacing) * (h-spacing)
-    var numberOfPixels = Math.floor(windowArea / pixelSize)
+    var pixelWidth = 20;
+    var pixelHeight = 20;
+    var windowArea = (w) * (h)
+    var numberOfPixels = Math.floor(w / pixelWidth) * Math.floor(h / pixelHeight)
+
     createBoard(numberOfPixels)
 }
-var createBoard = function(numOfPixels){
+var createBoard = (numOfPixels) => {
     document.querySelector(".div-container").innerHTML = ""
     for (i=0; i < numOfPixels; i++) {
         var divObjects = document.createElement('div')
         var divContainer = document.querySelector(".div-container")
         divObjects.classList.add("style","pixel")
+        divContainer.addEventListener('mouseover', handleClick)
         divContainer.appendChild(divObjects)
     }
-    // debugger
-    pixels = reallocatePixels()
+    pixels = reallocatePixels.call()
 }
 
-var reallocatePixels = function() {
-    return document.querySelectorAll(".pixel")
-}
+var reallocatePixels = () => document.querySelectorAll(".pixel")
 
     
 displayWindowSize.call()
@@ -42,7 +35,7 @@ userColor = "grey"
 
 // // creates container
 
-var changeColor = function(event) {
+var changeColor = (event) => {
     userColor = colorInput.value
     event.preventDefault()
     colorButton.style.background = userColor
@@ -51,20 +44,22 @@ var changeColor = function(event) {
 colorButton.addEventListener('click', changeColor)
 
 
-var handleClick = function(event){
-    event.target.style.background = userColor
+var handleClick = (event) => { 
+    if (event.target.classList.contains("pixel")) {
+        event.target.style.background = userColor
+    }
 }
 
 var movie_button = document.querySelector('#movie-btn')
 
 var clearPixels = function() {
-    pixels.forEach(function(pixel) {
+    pixels.forEach((pixel) => {
         pixel.classList.remove("style")
         pixel.classList.add("clear")
     })
 }
 
-var movieClick = function (event) {
+var movieClick = (event) => {
     event.preventDefault()
     var searchTerm = document.querySelector("#search-field").value
     var div = document.querySelector(".div-container")
@@ -72,7 +67,7 @@ var movieClick = function (event) {
         url: `http://www.omdbapi.com/?t=${ searchTerm }&apikey=2f6435d9&page=1`
     }
     
-    $.ajax(options).done(function(resp) {
+    $.ajax(options).done((resp) => {
         clearPixels()
         div.classList.add("background")
         div.style.backgroundImage = `url('${resp.Poster}')`
